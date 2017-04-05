cb8ab26117668d12571ef3f60f00bb58c4b48451
Edward Burton <flyingearl@gmail.com>
2017-04-05 23:15:52 +0100

    Login
    Added the login form.

--- app/src/renderer/app/auth/components/Login.vue
+++ app/src/renderer/app/auth/components/Login.vue
@@ -1,5 +1,51 @@
 <template>
-    <p>login</p>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Login</div>
+                <div class="panel-body">
+                    <div class="alert alert-danger" v-if="errors.root">
+                        {{ errors.root }}
+                    </div>
+                    <form class="form-horizontal" role="form" @submit.prevent="submit">
+                        <div class="form-group" :class="{ 'has-error': errors.email }">
+                            <label for="email" class="col-md-4 control-label">Email address</label>
+
+                            <div class="col-md-6">
+                                <input type="email" id="email" class="form-control" name="email" autofocus
+                                       v-model="email">
+
+                                <span class="help-block" v-if="errors.email">
+                                    {{ errors.email[0] }}
+                                </span>
+                            </div>
+                        </div>
+
+                        <div class="form-group" :class="{ 'has-error': errors.password }">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input type="password" id="password" class="form-control" name="password"
+                                       v-model="password">
+
+                                <span class="help-block" v-if="errors.password">
+                                    {{ errors.password[0] }}
+                                </span>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Login
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
